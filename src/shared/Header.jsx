e71f44868a7fffabc6fd00b3f12fb652a981a4e3
Sergey Vasin <vvs221@bk.ru>
2020-08-17 21:37:50 +0300

    hmr error

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,9 +1,12 @@
+import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-export default function Header() {
+function HeaderComponent() {
   return (
     <header>
-      <h1>Reddit hello</h1>
+      <h1>Reddit fsfsfs</h1>
     </header>
   );
 }
+
+export const Header = hot(HeaderComponent);
