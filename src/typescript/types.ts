60bcf0899506cd5ad2cb53de721d5948e734bb4f
servasin <vvs221@bk.ru>
2020-08-22 02:21:06 +0300

    Array types

--- src/typescript/types.ts
+++ src/typescript/types.ts
@@ -27,3 +27,31 @@ const result2 = parseInt(tsString) - tsNumber;
 if (typeof tsString === 'number') {
   const result2 = tsString - tsNumber; 
 }
+
+// Union type
+const strOrNumber: string | number = '22';
+
+// Type alias 
+type StrOrNumber = string | number;
+
+const strOrNumber1: string | number = '22';
+const strOrNumber2: string | number = '22';
+const strOrNumber3: string | number = '22';
+const strOrNumber4: string | number = '22';
+
+type AllJsSimpleTypes = string | number | [] | object | undefined | null | void | symbol;
+
+// Array
+const tsArray: number [] = [1, 2, 3];
+const tsArrayGeneric: Array<number> = [1, 2, 3];
+
+const unionArray: (string | number)[] = [1, '2', 3, '4'];
+const unionArrayGeneric: Array<string | number> = [1, '2', 3, '4'];
+
+// Tuple
+const myTuple: [number, string] = [1, '2'];
+const [val1, val2] = myTuple;
+
+// const [ state, setState ] = useState();
+
+type StrangeTsTypes = any | unknown | never;
