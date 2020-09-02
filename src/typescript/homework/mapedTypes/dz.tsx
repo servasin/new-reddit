110cbaf70c958bd7b61f161bafd811f7a66a76be
servasin <vvs221@bk.ru>
2020-09-02 21:51:39 +0300

    maped types try 2

--- src/typescript/homework/mapedTypes/dz.tsx
+++ src/typescript/homework/mapedTypes/dz.tsx
@@ -12,10 +12,18 @@ interface IProps {
   firstProp: string
 }
 
+// const t = TMyType<typeof HomeComponent>;
+
 type TMyType<T> = {
   [N in keyof T]: T[N] extends React.Component ? TMyType<T[N]> : T[N]
 }
 
-// const t = TMyType<typeof HomeComponent>;
+type test1 = TMyType<typeof HomeComponent>;
+type test2 = TMyType<IProps>;
 
+const c: test1 = HomeComponent
 
+type TPropType<T> = T extends React.Component<infer E> ? E : T
+
+// ???
+type test3 = TPropType<typeof HomeComponent>;
