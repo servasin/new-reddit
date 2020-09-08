884f5c6eaca1b89ab753e5f23cefdd7459d119f8
servasin <vvs221@bk.ru>
2020-09-08 16:47:08 +0300

    maped types try 6

--- src/typescript/homework/mapedTypes/dz.tsx
+++ src/typescript/homework/mapedTypes/dz.tsx
@@ -25,22 +25,27 @@ type TMyType<T> = T extends React.ComponentType<infer P> ? P : never;
 type t = TMyType<typeof HomeComponent>;
 type t2 = TMyType<typeof GreetingComponent>;
 
+// 6.
 
+// type TDivElement = JSX.IntrinsicElements['div'];
 
-type TDivElement = JSX.IntrinsicElements['div']
-// type TGetJSXPropsProp<T extends keyof JSX.IntrinsicElements> = {
-//   [N in keyof T]: T[N]  extends keyof TDivElement
+// type TDivProps = TGetJSXPropsProp<'div'>
+
+// const props: TDivProps = {
+//   some: '1233' ,// throw error потому что не содержится в атрибутах div
+//   className: 'handler' // не выкидывает ошибку так как валидно для div элемента
 // }
 
-type TGetJSXPropsProp<T extends keyof JSX.IntrinsicElements> = {
-  [N in keyof T]: T[N]  extends keyof React.DetailedHTMLProps<React.HTMLAttributes<TDivElement>, TDivElement>
-}
+// Как мен написать "ИЛИ" в TypeScript?
+type filterHTMLAttr<T> = T extends keyof React.DOMAttributes<T> | React.ClassAttributes<T> ? never : T
+type TGetJSXPropsProp<tag> = tag extends keyof JSX.IntrinsicElements ? filterHTMLAttr<JSX.IntrinsicElements[tag]> : {};
 
-type TDivProps = TGetJSXPropsProp<'div'>
+const propsInput1: TGetJSXPropsProp<'input'> = {
+  required: true,
+  children: 'dsfdsf',
+};
 
-const props: TDivProps = {
-  some: '1233', // throw error потому что не содержится в атрибутах div
-  className: 'handler' // не выкидывает ошибку так как валидно для div элемента
-}
-
-console.log(props)
+const propsInput2: TGetJSXPropsProp<'input'> = {
+  required: true,
+  ref: 'asdsa',
+};
